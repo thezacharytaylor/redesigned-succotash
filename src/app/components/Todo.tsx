20da7114b006f7de3b3ad135b842bfe78c4df792
Zachary Taylor <zacharytaylor@tutamail.com>
2022-11-17 10:51:16 -0600

    Wrapping up input, delete, and complete toggle

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 function Todo() {
-  const [todos, setTodos] = React.useState();
-
-  const todoList = [
+  const [todoInput, setTodoInput] = React.useState('');
+  const [todos, setTodos] = React.useState([
     {
       id: 1,
       title: 'Learn React',
@@ -19,21 +18,66 @@ function Todo() {
       title: 'Learn Tailwind',
       completed: false,
     },
-  ];
+  ]);
 
   // const todoItems = () => [...todoList];
 
+  const handleTodoInput = event => {
+    setTodoInput(event.target.value);
+  };
+
+  const addTodo = event => {
+    event.preventDefault();
+
+    if (todoInput.trim().length === 0) {
+      return;
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: todos[todos.length - 1].id + 1,
+        title: todoInput,
+        completed: false,
+      },
+    ]);
+
+    setTodoInput('');
+  };
+
   const completeTodo = id => {
-    todoList[id].completed = true;
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = id => {
+    setTodos([...todos].filter(todo => todo.id !== id));
   };
 
   return (
     <div className="max-w-md p-10 my-5 bg-gray-200 border-solid rounded-md shadow-sm border-slate-50">
-      <input type="text" className="border-gray-500" />
+      <form action="#" onSubmit={addTodo}>
+        <input
+          type="text"
+          className="border-gray-500"
+          value={todoInput}
+          onChange={handleTodoInput}
+        />
+        <button type="submit" className="px-2 text-white bg-gray-700">
+          +
+        </button>
+      </form>
       <hr className="my-4 border-gray-800 border-solid" />
       <h2 className="mb-4 text-lg font-bold">ToDo:</h2>
       <ul className="">
-        {todoList.map((todo, index) => (
+        {todos.map((todo, index) => (
           <li
             key={todo.id}
             className={`ml-3 flex items-center ${
@@ -46,8 +90,13 @@ function Todo() {
               onChange={() => completeTodo(todo.id)}
               checked={todo.completed ? true : false}
             />
-            <label>Learn React</label>
-            <button className="inline-block ml-auto mr-3">X</button>
+            <label>{todo.title}</label>
+            <button
+              className="inline-block ml-auto mr-3"
+              onClick={() => deleteTodo(todo.id)}
+            >
+              X
+            </button>
           </li>
         ))}
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: ['line-through'],
   theme: {
     extend: {},
   },
