8bbf3032a49daf6941d15ceb763d4064aad46ee6
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-27 16:36:14 -0600

    Cleaning up old MUI stuff

--- src/app/components/YearDisplay.tsx
+++ src/app/components/YearDisplay.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import dayjs from 'dayjs';
 import useFirstThursday from 'app/hooks/useFirstThursday';
 
 const YearDisplay = props => {
-  const [cupDate, setCupDate] = useFirstThursday();
+  const [cupDate] = useFirstThursday();
 
   return <>{`${cupDate['date']} ${props.children}`}</>;
 };
--- src/app/layouts/dashboard/index.tsx
+++ src/app/layouts/dashboard/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
 import DashboardSidebarNavigation from './dashboard-sidebar-nav';
 
 type Props = { children: React.ReactNode };
 const Dashboard = ({ children }: Props) => {
   return (
-    <Grid
-      container
-      direction="row"
-      justify="flex-start"
-      alignItems="flex-start"
-    >
-      <DashboardSidebarNavigation /> {children}
-    </Grid>
+    // <Grid
+    //   container
+    //   direction="row"
+    //   justify="flex-start"
+    //   alignItems="flex-start"
+    // >
+    //   <DashboardSidebarNavigation /> {children}
+    // </Grid>
+    <></>
   );
 };
 
