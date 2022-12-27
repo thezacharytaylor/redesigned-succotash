12a5c0b616e5704ad5997a4cfa9fb97173ab923e
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-27 15:54:48 -0600

    Making year a custom hook

--- src/app/components/YearDisplay.tsx
+++ src/app/components/YearDisplay.tsx
@@ -1,61 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
-
-const initialDate = {
-  month: '10',
-  year: '2022',
-};
+import useFirstThursday from 'app/hooks/useFirstThursday';
 
 const YearDisplay = props => {
-  const [cupDate, setCupDate] = useState(initialDate);
+  const [cupDate, setCupDate] = useFirstThursday();
 
-  useEffect(() => {
-    // Move this into a custom hook
-    const currentMonth = dayjs().month();
-    let cupYear = dayjs().year().toString();
-    let setNextYear = false;
-
-    // if (currentMonth > 9) {
-    //   setNextYear = true;
-    // }
-    // let testIncrem = 4;
-    // let testDay = dayjs(`${dayjs().year()}-10-${testIncrem}`).format('dddd');
-    // console.log(testDay === dayjs().format('dddd') + ' ' + testBool);
-
-    switch (currentMonth) {
-      case 9:
-        let firstThursday = 0;
-        let locatedFirstThursday = false;
-
-        for (let i = 1; i >= 9; i++) {
-          let sampleDay = dayjs(`${dayjs().year()}-10-${i}`).format('dddd');
-          if (!locatedFirstThursday) {
-            if (sampleDay === 'Thursday') firstThursday = i;
-          }
-        }
-
-        if (firstThursday < dayjs().day()) {
-          setNextYear = true;
-        }
-
-        break;
-      case 10:
-      case 11:
-        setNextYear = true;
-        break;
-      default:
-        break;
-    }
-
-    if (setNextYear) {
-      const nextYear = dayjs().add(1, 'year');
-      cupYear = nextYear.year().toString();
-    }
-
-    setCupDate({ ...cupDate, year: cupYear });
-  }, []);
-
-  return <>{`${cupDate.year} ${props.children}`}</>;
+  return <>{`${cupDate['date']} ${props.children}`}</>;
 };
 
 export default YearDisplay;
---
+++ src/app/hooks/useFirstThursday.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
+
+function useFirstThursday(initialDate = { date: '', override: false }) {
+  const [dateInfo, setDateInfo] = useState(initialDate);
+
+  useEffect(() => {
+    let cupYear = dayjs().year().toString();
+    let overrideSwitch = false;
+
+    if (dateInfo['date'].length > 0) {
+      overrideSwitch = true;
+      cupYear = initialDate['date'];
+    }
+
+    if (!dateInfo['override']) {
+      const currentMonth = dayjs().month();
+      let nextYear = false;
+
+      switch (currentMonth) {
+        case 9:
+          let firstThursday = 0;
+          let locatedFirstThursday = false;
+
+          for (let i = 1; i >= 9; i++) {
+            let sampleDay = dayjs(`${dayjs().year()}-10-${i}`).format('dddd');
+            if (!locatedFirstThursday) {
+              if (sampleDay === 'Thursday') firstThursday = i;
+            }
+          }
+
+          if (firstThursday < dayjs().day()) {
+            nextYear = true;
+          }
+
+          break;
+        case 10:
+        case 11:
+          nextYear = true;
+          break;
+        default:
+          break;
+      }
+
+      if (nextYear) {
+        const nextYear = dayjs().add(1, 'year');
+        cupYear = nextYear.year().toString();
+      }
+    }
+
+    setDateInfo({ ...dateInfo, date: cupYear, override: overrideSwitch });
+  }, []);
+
+  return [dateInfo, setDateInfo];
+}
+
+export default useFirstThursday;
