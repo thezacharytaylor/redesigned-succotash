f5448e9b0f95b18559e0e5c1020f41c374bb2b29
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-28 15:55:04 -0600

    Tweaking look of prediction

--- src/app/components/Form/index.tsx
+++ src/app/components/Form/index.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import Input from './Input';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
+import { useRef, useState } from 'react';
 
 const Form = ({ submit, input, info, golfers }) => {
-  const handlePrediction = () => {
-    return <p className="absolute px-2 bg-white">Hi yea</p>;
+  // const [valueState, setValueState] = useState(info.name);
+
+  const handleUpdate = (event, golferName) => {
+    event.preventDefault();
+    // nameField = golfer.name;
+    // console.log(nameField);
+    // if (nameField.current !== undefined) {
+    //   // nameField.current = golferName;
+    //   console.log(nameField.current);
+    // }
+    // return <p className="absolute px-2 bg-white">Hi yea</p>;
   };
 
   return (
     <>
       <form action="#" onSubmit={submit} className="relative flex">
         <Input name="name" placeholder="Name" value={info.name} func={input} />
-        <CSSTransition
-          in={info.name.length > 0 && golfers.length > 0}
-          timeout={300}
-          classNames="slide-vertical"
-          unmountOnExit
-        >
-          <ul className="absolute p-2 bg-white -bottom-10">
-            {golfers.map((golfer, index) => {
-              return <li key={index}>{golfer.name}</li>;
-            })}
-          </ul>
-        </CSSTransition>
+        <div className="absolute bottom-0 w-full">
+          <CSSTransition
+            in={info.name.length > 0 && golfers.length > 0}
+            timeout={300}
+            classNames="slide-vertical"
+            unmountOnExit
+          >
+            <ul className={`absolute bg-white`}>
+              {golfers.map((golfer, index) => {
+                return (
+                  <li key={index}>
+                    <button
+                      className="p-2 bg-transparent border-0 hover:bg-gray-300"
+                      onClick={event => handleUpdate(event, golfer.name)}
+                      name="name"
+                      value={golfer.name}
+                    >
+                      {golfer.name}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </CSSTransition>
+        </div>
         <Input name="score" placeholder={1} value={info.score} func={input} />
         <button
           type="submit"
