0aa1c871386c41d1688df70c83d3cbe097f032c1
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-28 17:21:34 -0600

    Book confusing redux stuff

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,33 +1,22 @@
-import { configureStore, StoreEnhancer } from '@reduxjs/toolkit';
-import { createInjectorsEnhancer } from 'redux-injectors';
-import createSagaMiddleware from 'redux-saga';
-
+/*Create the store with dynamic reducers */
+import { configureStore } from '@reduxjs/toolkit';
+import { forceReducerReload } from 'redux-injectors';
 import { createReducer } from './reducers';
 
 export function configureAppStore() {
-  const reduxSagaMonitorOptions = {};
-  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
-  const { run: runSaga } = sagaMiddleware;
-
-  // Create the store with saga middleware
-  const middlewares = [sagaMiddleware];
-
-  const enhancers = [
-    createInjectorsEnhancer({
-      createReducer,
-      runSaga,
-    }),
-  ] as StoreEnhancer[];
-
   const store = configureStore({
+    /*reducer is required. middleware, devTools, and the rest are optional */
     reducer: createReducer(),
-    middleware: defaultMiddleware => [...defaultMiddleware(), ...middlewares],
-    devTools:
-      /* istanbul ignore next line */
-      process.env.NODE_ENV !== 'production' ||
-      process.env.PUBLIC_URL.length > 0,
-    enhancers,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({ serializableCheck: false }),
+    devTools: process.env.NODE_ENV !== 'production',
   });
-
+  /* Make reducers hot reloadable, see http://mxs.is/googmo istanbul ignore next */ if (
+    module.hot
+  ) {
+    module.hot.accept('./reducers', () => {
+      forceReducerReload(store);
+    });
+  }
   return store;
 }
