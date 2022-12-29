6466b627fd1ec956a6b916978e7de28f048e2f76
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-29 11:27:17 -0600

    Ignoring tslint warning in custom hook

--- src/app/hooks/useFirstThursday.tsx
+++ src/app/hooks/useFirstThursday.tsx
@@ -5,52 +5,57 @@ function useFirstThursday(initialDate = { date: '', override: false }) {
   const [dateInfo, setDateInfo] = useState(initialDate);
 
   // Ideally move this out of a useEffect.
-  useEffect(() => {
-    let cupYear = dayjs().year().toString();
-    let overrideSwitch = false;
+  useEffect(
+    () => {
+      let cupYear = dayjs().year().toString();
+      let overrideSwitch = false;
 
-    if (dateInfo['date'].length > 0) {
-      overrideSwitch = true;
-      cupYear = initialDate['date'];
-    }
+      if (dateInfo['date'].length > 0) {
+        overrideSwitch = true;
+        cupYear = initialDate['date'];
+      }
 
-    if (!dateInfo['override']) {
-      const currentMonth = dayjs().month();
-      let nextYear = false;
+      if (!dateInfo['override']) {
+        const currentMonth = dayjs().month();
+        let nextYear = false;
 
-      switch (currentMonth) {
-        case 9:
-          let firstThursday = 0;
-          const locatedFirstThursday = false;
+        switch (currentMonth) {
+          case 9:
+            let firstThursday = 0;
+            const locatedFirstThursday = false;
 
-          for (let i = 1; i >= 9; i++) {
-            const sampleDay = dayjs(`${dayjs().year()}-10-${i}`).format('dddd');
-            if (!locatedFirstThursday) {
-              if (sampleDay === 'Thursday') firstThursday = i;
+            for (let i = 1; i >= 9; i++) {
+              const sampleDay = dayjs(`${dayjs().year()}-10-${i}`).format(
+                'dddd',
+              );
+              if (!locatedFirstThursday) {
+                if (sampleDay === 'Thursday') firstThursday = i;
+              }
             }
-          }
 
-          if (firstThursday < dayjs().day()) {
+            if (firstThursday < dayjs().day()) {
+              nextYear = true;
+            }
+
+            break;
+          case 10:
+          case 11:
             nextYear = true;
-          }
+            break;
+          default:
+            break;
+        }
 
-          break;
-        case 10:
-        case 11:
-          nextYear = true;
-          break;
-        default:
-          break;
+        if (nextYear) {
+          const nextYear = dayjs().add(1, 'year');
+          cupYear = nextYear.year().toString();
+        }
       }
 
-      if (nextYear) {
-        const nextYear = dayjs().add(1, 'year');
-        cupYear = nextYear.year().toString();
-      }
-    }
-
-    setDateInfo({ ...dateInfo, date: cupYear, override: overrideSwitch });
-  }, []);
+      setDateInfo({ ...dateInfo, date: cupYear, override: overrideSwitch });
+    }, // eslint-disable-next-line
+    [],
+  );
 
   return [dateInfo, setDateInfo];
 }
