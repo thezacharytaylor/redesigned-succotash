cef334de81a8fdc4077a6ffc13e7071900ffd391
Zachary Taylor <zacharytaylor@tutamail.com>
2023-01-20 15:15:34 -0600

    test: Reworking test index to main app.

--- src/app/__tests__/index.test.tsx
+++ src/app/__tests__/index.test.tsx
@@ -1,10 +1,24 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import { NavBar } from 'app/components/NavBar';
 import { App } from '../index';
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
+import { configureAppStore } from 'store/configureStore';
+
+const store = configureAppStore();
 
 test('should render and match the snapshot', () => {
-  render(<NavBar />);
+  render(
+    <Provider store={store}>
+      <HelmetProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </HelmetProvider>
+    </Provider>,
+  );
   const headerElement = screen.getByText('Kinetic Cup');
   expect(headerElement).toBeInTheDocument();
 });
