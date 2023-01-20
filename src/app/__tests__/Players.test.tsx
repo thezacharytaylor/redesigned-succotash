e291f956dc6bc16aed0fee9e4e4987f40ae6724b
Zachary Taylor <zacharytaylor@tutamail.com>
2023-01-20 17:02:05 -0600

    Adding userEvent to Players test

--- src/app/__tests__/Players.test.tsx
+++ src/app/__tests__/Players.test.tsx
@@ -1,20 +1,38 @@
 import renderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
 import Players from 'app/components/Players';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { configureAppStore } from 'store/configureStore';
 
 const store = configureAppStore();
+const user = userEvent.setup();
 
-test('should render and match the snapshot', () => {
-  const domTree = renderer
-    .create(
-      <Provider store={store}>
-        <HelmetProvider>
-          <Players />
-        </HelmetProvider>
-      </Provider>,
-    )
-    .toJSON();
-  expect(domTree).toMatchSnapshot(); //
+describe('Load app', () => {
+  test('should render and match the snapshot', () => {
+    const domTree = renderer
+      .create(
+        <Provider store={store}>
+          <HelmetProvider>
+            <Players />
+          </HelmetProvider>
+        </Provider>,
+      )
+      .toJSON();
+    expect(domTree).toMatchSnapshot(); //
+  });
+});
+
+describe('Add player', () => {
+  test('Should add fake player to leaderboard', () => {
+    const domTree = renderer
+      .create(
+        <Provider store={store}>
+          <HelmetProvider>
+            <Players />
+          </HelmetProvider>
+        </Provider>,
+      )
+      .toJSON();
+  });
 });
