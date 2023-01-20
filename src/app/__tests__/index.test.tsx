4433a7451e5612c1f574a188c2bd96a286c7767b
Zachary Taylor <zacharytaylor@tutamail.com>
2023-01-20 14:52:08 -0600

    Cleaning up test index.

--- src/app/__tests__/index.test.tsx
+++ src/app/__tests__/index.test.tsx
@@ -1,20 +1,10 @@
-import { render } from '@testing-library/react';
-import * as React from 'react';
-import { createRenderer } from 'react-test-renderer/shallow';
+import { render, screen } from '@testing-library/react';
+
 import { NavBar } from 'app/components/NavBar';
-
 import { App } from '../index';
-
-// const renderer = createRenderer();
-
-// describe('<App />', () => {
-//   it('should render and match the snapshot', () => {
-//     render(<App />);
-//     // const renderedOutput = renderer.getRenderOutput();
-//     // expect(renderedOutput).toMatchSnapshot();
-//   });
-// });
 
 test('should render and match the snapshot', () => {
   render(<NavBar />);
+  const headerElement = screen.getByText('Kinetic Cup');
+  expect(headerElement).toBeInTheDocument();
 });
