6175618c1438f8d82faa3d4aa6d058dc5a6bd4d9
Zachary Taylor <zacharytaylor@tutamail.com>
2023-01-24 16:19:23 -0600

    improvement: Linking up golfers

--- src/app/components/Leaderboard/Label.tsx
+++ src/app/components/Leaderboard/Label.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const Label = ({
   index,
   player,
@@ -10,23 +11,49 @@ const Label = ({
 }) => {
   const buildResult = () => {
     if (nameCol) {
-      return (
-        <div
-          data-rank={index + 1}
-          role="note"
-          onKeyDown={event => keyDown(event)}
-          onKeyUp={event => keyUp(event)}
-          tabIndex={focusedRow === player ? 0 : -1}
-          ref={elementRef => {
-            rowRefFunc(elementRef);
-          }}
-          aria-label={`Rank ${index + 1} ${player['name']} scored ${
-            player['score']
-          } on ${player['date']}`}
-        >
-          {children}
-        </div>
-      );
+      if (player['previousPlayer']) {
+        const urlSuffix = player['name']
+          .toLowerCase()
+          .replace(/[!@#$%^&*._,]/g, '')
+          .replace(/\s/g, '-');
+        return (
+          <>
+            <a
+              data-rank={index + 1}
+              href={`https://kineticcup.com/player/${urlSuffix}`}
+              onKeyDown={event => keyDown(event)}
+              onKeyUp={event => keyUp(event)}
+              tabIndex={focusedRow === player ? 0 : -1}
+              ref={elementRef => {
+                rowRefFunc(elementRef);
+              }}
+              aria-label={`Rank ${index + 1} ${player['name']} scored ${
+                player['score']
+              } on ${player['date']}`}
+            >
+              {children}
+            </a>
+          </>
+        );
+      } else {
+        return (
+          <div
+            data-rank={index + 1}
+            role="note"
+            onKeyDown={event => keyDown(event)}
+            onKeyUp={event => keyUp(event)}
+            tabIndex={focusedRow === player ? 0 : -1}
+            ref={elementRef => {
+              rowRefFunc(elementRef);
+            }}
+            aria-label={`Rank ${index + 1} ${player['name']} scored ${
+              player['score']
+            } on ${player['date']}`}
+          >
+            {children}
+          </div>
+        );
+      }
     } else {
       return <>{children}</>;
     }
